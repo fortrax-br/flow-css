40c096ae1ebf0c3b5178cc7be01e8dd3d11d7b10
DKSecurity99 <dksecurity@gmail.com>
2020-11-30 18:36:50 -0300

    feat: recriado as classes utilitarias, já utilizando os mixins de automatização

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,19 +1,22 @@
 'use strict'
 
 module.exports = ctx => ({
-  map: ctx.file.dirname.includes('examples') ?
-    false :
-    {
-      inline: false,
-      annotation: true,
-      sourcesContent: true
-    },
-  plugins: {
-    cssnano: {
+  console.log(ctx);
+  return {
+    map: ctx.file.dirname.includes('examples') ?
+      false :
+      {
+        inline: false,
+        annotation: true,
+        sourcesContent: true
+      },
+    plugins: {
+      cssnano: {
       cascade: false,
     },
-    autoprefixer: {
+      autoprefixer: {
       cascade: false
+      }
     }
   }
 });
