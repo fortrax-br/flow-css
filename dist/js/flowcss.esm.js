8ee75119eea80bd66f39d7a9e6550e64a933e51f
DKSecurity99 <dksecurity@gmail.com>
2020-12-21 20:16:13 -0300

    feat(js): the current states of the javascript components are built

--- dist/js/flowcss.esm.js
+++ dist/js/flowcss.esm.js
@@ -3,12 +3,53 @@
   * Copyright 2020-2020 Fortrax
   * Licensed under MIT (https://github.com/fortrax/flowcss/blob/main/LICENSE)
   */
-function testing() {
-  alert('testing script compile');
+function selectAllElements(selector = null, documentSelect = document) {
+  if (selector) {
+    const domElements = documentSelect.querySelectorAll(selector);
+    return domElements.length > 0 ? domElements : [];
+  }
+
+  return [];
 }
 
+const DATA_ALERT_HIDE = '[data-flow-hide="alert"]';
+const CLASS_ALERT = 'alert';
+const ALERT_EVENT = 'click';
+
+class Alert {
+  constructor(element = DATA_ALERT_HIDE) {
+    this.element = element;
+  }
+
+  _getMainElement(childElement) {
+    return childElement.closest(`.${CLASS_ALERT}`);
+  }
+
+  _destroyDomElement(element) {
+    if (element.parentNode) {
+      element.parentNode.removeChild(element);
+    }
+  }
+
+  _closeAlert(closeElement) {
+    const mainElement = this._getMainElement(closeElement);
+
+    this._destroyDomElement(mainElement);
+  }
+
+  execute() {
+    const domElements = selectAllElements(this.element);
+    domElements.forEach(element => {
+      element.addEventListener(ALERT_EVENT, () => this._closeAlert(element), false);
+    });
+  }
+
+}
+
+window.addEventListener('load', () => new Alert().execute(), false);
+
 var index_esm = {
-  testing: testing()
+  Alert
 };
 
 export default index_esm;
