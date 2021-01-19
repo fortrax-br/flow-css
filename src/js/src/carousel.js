a0aaca780a52f20dd2637dab01c6e2762f8d9f1b
DKSecurity99 <dksorteio@gmail.com>
2021-01-19 11:46:22 -0300

    feat(js/carousel): finished creating the carousel control, more optimizations in progress

--- src/js/src/carousel.js
+++ src/js/src/carousel.js
@@ -1,13 +1,12 @@
 import ExtendComponent from './extends/extendComponent';
 import { selectAllElements } from './DOM/selectDomElements';
 
-const CAROUSEL = '.carousel';
-const CAROUSEL_DATA_TARGET = '[data-tw-target="carousel"]';
-const CAROUSEL_ITEM = '.carousel-item';
+const CAROUSEL_DATA_TARGET = '[data-fw-target="carousel"]';
 const CAROUSEL_DOT = '.carousel-dot';
-const CAROUSEL_CONTROL = '.carousel-control';
 const CAROUSEL_CONTROL_PREV = '.carousel-control-prev';
 const CAROUSEL_CONTROL_NEXT = '.carousel-control-next';
+const CAROUSEL_ACTIVE = 'active';
+
 const COMPONENT_KEY = 'fw.carousel';
 const SLIDE_DELAY = 2000;
 
@@ -18,21 +17,20 @@ class Carousel extends ExtendComponent {
     this._childElement = childElement;
     this._allSlides = Array.from(this._childElement.children);
     this._currentSlide = 0;
-    this._isWithControls = Boolean(this._element.dataset.twControls);
-    this._isWithDots = Boolean(this._element.dataset.twDots);
+    this._isWithControls = Boolean(this._element.dataset.fwControls == 'true');
+    this._isWithDots = Boolean(this._element.dataset.fwDots == 'true');
     this._dotElements = [];
     this._controlElements = [];
-    this._interval;
   }
 
   _moveSlide(targetSlide) {
     this._allSlides.forEach((slide, index) => {
-      slide.classList.remove('active');
-      this._dotElements[index].classList.remove('active');
+      slide.classList.remove(CAROUSEL_ACTIVE);
+      this._isWithDots && this._dotElements[index].classList.remove(CAROUSEL_ACTIVE);
     });
 
-    this._allSlides[targetSlide].classList.add('active');
-    this._dotElements[targetSlide].classList.add('active');
+    this._allSlides[targetSlide].classList.add(CAROUSEL_ACTIVE);
+    this._isWithDots && this._dotElements[targetSlide].classList.add(CAROUSEL_ACTIVE);
   }
 
   _incrementAndMoveSlide(targetSlide) {
@@ -41,8 +39,8 @@ class Carousel extends ExtendComponent {
   }
 
   _configureDotElements() {
-    this._dotElements = selectAllElements(CAROUSEL_DOT);
-    this._dotElements[0].classList.add('active');
+    this._dotElements = selectAllElements(CAROUSEL_DOT, this._element);
+    this._dotElements[0].classList.add(CAROUSEL_ACTIVE);
 
     this._dotElements.forEach((dot, index) => {
       dot.addEventListener('click', () => this._incrementAndMoveSlide(index));
@@ -50,10 +48,10 @@ class Carousel extends ExtendComponent {
   }
 
   _configureControlElements() {
-    const controlPrevSlide = selectAllElements(CAROUSEL_CONTROL_PREV);
-    const controlNextSlide = selectAllElements(CAROUSEL_CONTROL_NEXT);
-    
-    controlPrevSlide.forEach((control, index) => {
+    const controlPrevSlide = selectAllElements(CAROUSEL_CONTROL_PREV, this._element);
+    const controlNextSlide = selectAllElements(CAROUSEL_CONTROL_NEXT, this._element);
+
+    controlPrevSlide.forEach(control => {
       control.addEventListener('click', () => {
         let prevIndex = this._currentSlide > 0 
           ? this._currentSlide-1
@@ -63,8 +61,7 @@ class Carousel extends ExtendComponent {
       });
     });
 
-    controlNextSlide.forEach((control, index) => {
-    if (control.closest(`#${this._element.id}`)) {
+    controlNextSlide.forEach(control => {
       control.addEventListener('click', () => {
         let nextIndex = this._currentSlide < this._allSlides.length-1 
           ? this._currentSlide + 1
@@ -72,36 +69,37 @@ class Carousel extends ExtendComponent {
 
         this._incrementAndMoveSlide(nextIndex);
       });
-    }});
+    });
+  }
+
+  _animateCarousel() {
+    if (this._currentSlide === this._allSlides.length) {
+      this._currentSlide = 0;
+    }
+      
+    this._moveSlide(this._currentSlide);
+    this._currentSlide+=1;
+
+    setTimeout(() => {
+      requestAnimationFrame(() => this._animateCarousel());
+    }, SLIDE_DELAY);
   }
 
   execute() {
-    if (this._isWithDots) {
-      this._configureDotElements();
-    }
+    this._isWithDots && this._configureDotElements();
     
-    let interval = setInterval(() => {
-      if (this._currentSlide === this._allSlides.length) {
-        this._currentSlide = 0;
-      }
-      
-      this._moveSlide(this._currentSlide);
-      this._currentSlide+=1;
-    }, SLIDE_DELAY);
-
-
-    if (this._isWithControls) {
-      this._configureControlElements();
-      clearInterval(interval);
-    } 
-  }  
+    this._isWithControls 
+      ? this._configureControlElements()
+      : this._animateCarousel();
+  }
 }
 
 window.addEventListener('load', () => {
-  const elements = selectAllElements(CAROUSEL);
-  const childElements = selectAllElements(CAROUSEL_DATA_TARGET);
+  const childElements = Array.from(selectAllElements(CAROUSEL_DATA_TARGET));
   
-  elements.forEach((element, index) => {
+  const elementsID = childElements.map(child => document.querySelector(`#${child.dataset.fwToggle}`));
+
+  elementsID.forEach((element, index) => {
     new Carousel(
       element, 
       childElements[index], 
