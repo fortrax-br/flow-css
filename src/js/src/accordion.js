145de91f1873e01b6d730efc1be0579f40918905
DKSecurity99 <dksorteio@gmail.com>
2021-02-08 08:46:27 -0300

    fix: removed error in the control of the accordion component

--- src/js/src/accordion.js
+++ src/js/src/accordion.js
@@ -16,7 +16,7 @@ class Accordion extends ExtendComponent {
     const buttons = document.querySelectorAll(ACCORDION_BUTTON);
 
     const buttonsAndElements = Array.prototype.map.call(buttons, button => {
-      const dataButtonTarget = button.dataset.flowTarget;
+      const dataButtonTarget = button.dataset.fwTarget;
       const element = document.querySelector(dataButtonTarget);
 
       return { button, element }
@@ -28,12 +28,12 @@ class Accordion extends ExtendComponent {
   _setClassesInElements(targetButton, targetElement) {
     const currentElement = document.querySelector(`.${ACCORDION_CLASS_SHOW}`);
     const currentButton = document.querySelector(`.${ACCORDION_BUTTON_CLICKED}`);
-
+    
     if (currentElement && currentButton) {
       currentElement.classList.remove(ACCORDION_CLASS_SHOW);
       currentButton.classList.remove(ACCORDION_BUTTON_CLICKED);
     }
-
+    
     if (!targetElement.classList.contains(ACCORDION_CLASS_SHOW)) {
       targetElement.classList.add(ACCORDION_CLASS_SHOW);
       targetButton.classList.add(ACCORDION_BUTTON_CLICKED);
@@ -41,7 +41,8 @@ class Accordion extends ExtendComponent {
   }
 
   _addEventInButton(button, element) {
-    button.addEventListener('click', () => this._setClassesInElements(button, element), true);
+    button.addEventListener('click', () => 
+      this._setClassesInElements(button, element), true);
   }
 
   execute() {
